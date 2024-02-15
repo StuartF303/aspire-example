6e1dfb47bd1dc21c240bd552aa22f266e96fc4f3
Stuart Fraser <stuart@wallet.services>
2024-02-15 16:18:02 +0000

    runs up

--- src/gwdae.apphost/Program.cs
+++ src/gwdae.apphost/Program.cs
@@ -2,9 +2,9 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var grpcService = builder.AddProject<gwdae.grpcservice>("grpcservice");
+var grpcService = builder.AddProject<gwdae_grpcservice>("grpcservice");
 
-builder.AddProject<gwdae.web>("webui")
+builder.AddProject<gwdae_web>("webui")
     .WithReference(grpcService);
 
 
